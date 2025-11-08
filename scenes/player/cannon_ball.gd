b52eae0a2d5e99a30e44f1d24dc979ed21b1d072
Jeff <jeffreysabol345@gmail.com>
2025-11-08 00:05:26 -0500

    Spawn smoke where the cannon muzzle fires

--- scenes/player/cannon_ball.gd
+++ scenes/player/cannon_ball.gd
@@ -1,8 +1,15 @@
+# cannon_ball.gd
+class_name CannonBall
 extends Node2D
 
 @export var speed: float = 375.0
+@onready var notifier: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
 
-func _physics_process(delta):
-	# Move forward relative to rotation
+func _ready() -> void:
+	if notifier:
+		notifier.screen_exited.connect(queue_free)
+	# Fail-safe in case it never leaves screen:
+	get_tree().create_timer(10.0).timeout.connect(queue_free)
+
+func _physics_process(delta: float) -> void:
 	position += Vector2.RIGHT.rotated(global_rotation) * speed * delta
- 
--- scenes/player/gun.gd
+++ scenes/player/gun.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
 @export var cannonball_scene: PackedScene
+@export var smoke_scene: PackedScene
 @export var shoot_speed: float = 375.0
 
-var cannon_sounds := [
+var cannon_sounds: Array[String] = [
 	"res://assets/sfx/Cannon/cannon_1.mp3",
 	"res://assets/sfx/Cannon/cannon_2.mp3",
 	"res://assets/sfx/Cannon/cannon_3.mp3",
@@ -12,18 +13,40 @@ var cannon_sounds := [
 	"res://assets/sfx/Cannon/cannon_6.mp3",
 ]
 
+@onready var gun_sprite: AnimatedSprite2D = $GunSprite
+@onready var boom: AudioStreamPlayer2D = $Boom
+@onready var muzzle: Marker2D = $Muzzle
+
 func fire() -> void:
-	$GunSprite.play()
+	gun_sprite.play()
 
-	var path = cannon_sounds.pick_random()
-	$Boom.stream = load(path)
-	$Boom.play()
+	var path: String = cannon_sounds.pick_random()
+	boom.stream = load(path) as AudioStream
+	boom.play()
 
+	# Smoke at muzzle (auto-free when animation ends)
+	if smoke_scene:
+		var smoke := smoke_scene.instantiate() as Node2D
+		if smoke:
+			smoke.global_position = muzzle.global_position
+			smoke.global_rotation = muzzle.global_rotation
+			get_tree().current_scene.add_child(smoke)
+
+		# Create a timer that removes the smoke after X seconds
+		var t := Timer.new()
+		t.wait_time = 0.6
+		t.one_shot = true
+		smoke.add_child(t)
+		t.start()
+		t.timeout.connect(func(): smoke.queue_free())
+
+	# small firing delay
 	await get_tree().create_timer(0.2).timeout
 
-	var ball := cannonball_scene.instantiate()
-	ball.global_position = $Muzzle.global_position
-	ball.global_rotation = $Muzzle.global_rotation
-
-	get_tree().current_scene.add_child(ball)
-	ball.speed = shoot_speed
+	# Spawn cannonball (typed)
+	if cannonball_scene:
+		var ball: = cannonball_scene.instantiate()
+		ball.global_position = muzzle.global_position
+		ball.global_rotation = muzzle.global_rotation
+		ball.speed = shoot_speed
+		get_tree().current_scene.add_child(ball)
