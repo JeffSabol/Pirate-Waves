f9834793ab1cdf15100ed642c0a9bbb50f32fc07
Jeff <jeffreysabol345@gmail.com>
2025-11-10 22:28:20 -0500

    Display the player gold in the trade menu

--- scenes/town/trade/trade.gd
+++ scenes/town/trade/trade.gd
@@ -1,5 +1,7 @@
 extends Control
 
+func _physics_process(delta):
+	$CanvasLayer/MenuBackground/PlayerNameLabel/PlayerGoldLabel.text = str($"../../PlayerBoat".gold)
 
 func _on_leave_pressed():
 	queue_free()
