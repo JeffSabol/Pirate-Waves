c22c429f5eddeb42c030088f47578acb7acc5f8f
Jeff <jeffreysabol345@gmail.com>
2025-11-15 17:24:59 -0500

    Remove inventory button code

--- scenes/game_scene/world/inventory_button.gd
+++ scenes/game_scene/world/inventory_button.gd
@@ -1,13 +1,7 @@
 extends TextureButton
 
 func _input(_event):
-	if Input.is_action_pressed("open_inv"):
-		if not $"../Inventory".is_visible_in_tree():
-			$"../Inventory".show()
-
-	if Input.is_action_just_released("open_inv"):
-		if $"../Inventory".is_visible_in_tree():
-			$"../Inventory".hide()
+	pass
 
 
 func _on_pressed():
