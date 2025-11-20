51b1dfc211a7b6613d53184f656b75843d8c5a64
Jeff <jeffreysabol345@gmail.com>
2025-11-19 19:35:18 -0500

    Add the new tavern assets. Replacing AI slop placeholders.

--- scenes/town/tavern/leave_area.gd
+++ scenes/town/tavern/leave_area.gd
@@ -3,25 +3,12 @@ extends ColorRect
 var pulse_tween: Tween
 
 func _on_mouse_entered():
-	# Stop any previous tween (in case of re-entry)
-	if pulse_tween and pulse_tween.is_running():
-		pulse_tween.kill()
-
-	# Create a looping tween that brightens and dims slowly
-	pulse_tween = create_tween().set_loops().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
-	pulse_tween.tween_property(self, "color", Color(1.0, 1.0, 0.0, 0.05), 1) # brighten over 1.5 sec
-	pulse_tween.tween_property(self, "color", Color(0.0, 0.0, 0.0, 0.0), 1) # dim over 1.5 sec
-	$FireplaceSound.play()
+	$"..".texture = load("res://assets/ui/town/tavern/TavernBackgroundLeave.png")
+	$Doorcreak.play()
 	
 func _on_mouse_exited():
-	# Stop the pulse tween and fade out gently
-	if pulse_tween and pulse_tween.is_running():
-		pulse_tween.kill()
-
-	var fade_tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
-	fade_tween.tween_property(self, "color", Color(0, 0, 0, 0), 1.0) # fade out slower
-	$FireplaceSound.stop()
-
+	$"..".texture = load("res://assets/ui/town/tavern/TavernBackgroundEmpty.png")
+	$Doorcreak.stop()
 
 func _on_gui_input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
